73d457667ce499183f5044decaa71d14cf654271
Bionca Bond <bioncab@zillowgroup.com>
2021-09-21 21:29:16 -0700

    rendered images with react

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import HornedBeasts from './HornedBeast';
+import data from './data.json'; 
+
 
 class main extends React.Component {
-  render() {
-     
-    return (
-      <>
-      <HornedBeasts title = 'bigfoot' img='test' description = 'stinky mythical land-walker' />
-      <HornedBeasts title = 'cracken' img='test' description = 'horned swimming creature' />
-      </>
-    )
-  }
-}
+  render() { 
 
+    let beastArr = []; 
+
+    data.forEach((beast, idx) => {
+    beastArr.push(
+      <HornedBeasts key={idx} image_url={beast.image_url} title={beast.title} description={beast.description} keyword={beast.keyword} horns={beast.horns}/>
+      )
+    })
+  
+  
+  return (
+      <> 
+      
+      {beastArr};
+       </>
+    )
+ 
+} 
+
+}
 export default main;
